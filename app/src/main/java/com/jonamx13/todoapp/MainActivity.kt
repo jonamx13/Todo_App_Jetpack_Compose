2a9bf65a728ec38ac60e6adb96c6129960af8b09
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2025-01-08 01:48:28 -0600

    Initial config of project

---
+++ app/src/main/java/com/jonamx13/todoapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.jonamx13.todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.jonamx13.todoapp.ui.theme.TodoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TodoAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HelloWorld(
+                        modifier = Modifier.fillMaxSize()
+                            .padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HelloWorld(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = "Hello, World!",
+        modifier = modifier
+    )
+}
